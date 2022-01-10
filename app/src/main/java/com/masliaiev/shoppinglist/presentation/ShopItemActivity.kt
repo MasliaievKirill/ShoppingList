74c0bb751b5b8f4aefc00ca8df811ef1d05b9a74
MasliaievKirill <kirillmaslyaev@gmail.com>
2022-01-10 16:43:27 +0200

    1.ShopItemActivity added 2.ShopItemViewModel added

---
+++ app/src/main/java/com/masliaiev/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.masliaiev.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.masliaiev.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
